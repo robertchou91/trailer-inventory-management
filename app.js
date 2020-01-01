026c52e68bc107b56778efc8f810a037e4482cae
robertchou91 <robertchou91@gmail.com>
2020-01-01 17:20:24 -0600

    Setting up (angularjs/bootstrap/jquery/css)

---
+++ app.js
@@ -0,0 +1,9 @@
+var app = angular.module("myApp", []);
+
+// $(document).ready(function () {  
+//     $('[data-toggle="popover"]').popover();  
+// }); 
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
