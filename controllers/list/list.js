69c2141275d5b16bc41b70375b1aa1e44dde8eee
robertchou91 <robertchou91@gmail.com>
2020-01-03 14:03:27 -0600

    working on delete

--- controllers/list/list.js
+++ controllers/list/list.js
@@ -1,12 +1,24 @@
-app.controller('listController', function($scope, $http) {
+app.controller('listController', function ($scope, $http) {
 
     $http.get("https://cargo-trailer-server.herokuapp.com/api/trailers")
-        .then(function(response) {
+        .then(function (response) {
             console.log(response.data);
             console.log("Successfully got Trailers");
-            $scope.trailers = response.data; 
-        }, function(response) {
+            $scope.trailers = response.data;
+        }, function (response) {
             console.log("Unable to get Trailers");
         });
-    
+
+    $scope.delete = function (object) {
+        $http({
+            url: 'https://cargo-trailer-server.herokuapp.com/api/trailers/' + object.id,
+            method: 'DELETE',
+        }).then(function (res) {
+            console.log(res.data);
+            console.log("trailer deleted");
+        }, function (error) {
+            console.log(error);
+            console.log("trailer not deleted");
+        });
+    };
 });
