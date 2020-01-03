412957dc21df3575b6c1f11b154ecdd25ea4e243
robertchou91 <robertchou91@gmail.com>
2020-01-02 18:39:43 -0600

    updating the list fields

--- controllers/list/list.js
+++ controllers/list/list.js
@@ -1,9 +1,9 @@
 app.controller('listController', function($scope) {
     $scope.trailers = [
-        {name: 'testing', dimension: '0-0-0', size: 'S', style: 'Style1', condition: 'BAD'},
-        {name: 'testing1', dimension: '1-1-1', size: 'S', style: 'Style2', condition: 'GOOD'},
-        {name: 'testing2', dimension: '2-2-2', size: 'S', style: 'Style3', condition: 'OK'},
-        {name: 'testing3', dimension: '3-3-3', size: 'S', style: 'Style4', condition: 'GOOD'},
-        {name: 'testing4', dimension: '4-4-4', size: 'S', style: 'Style5', condition: 'BAD'},
+        {modelnumber: 800800, dimension: '0x0x0', year: 1991, color: 'color1', payload: '100lbs', price: '$400', manufacturer: 'China', condition: 'BAD', quantity: 1},
+        {modelnumber: 812812, dimension: '1x1x1', year: 1993, color: 'color2', payload: '200lbs', price: '$300', manufacturer: 'Japan', condition: 'GOOD', quantity: 5},
+        {modelnumber: 833833, dimension: '2x2x2', year: 1899, color: 'color3', payload: '500lbs', price: '$400', manufacturer: 'Mexico', condition: 'OK', quantity: 10},
+        {modelnumber: 844844, dimension: '3x3x3', year: 2009, color: 'color4', payload: '300lbs', price: '$550', manufacturer: 'Mexico', condition: 'GOOD', quantity: 12},
+        {modelnumber: 855855, dimension: '4x4x4', year: 2019, color: 'color5', payload: '200lbs', price: '$300', manufacturer: 'USA', condition: 'BAD', quantity: 7},
     ];
 });
