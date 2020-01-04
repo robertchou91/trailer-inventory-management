afefa6d6ece416bcaca0e21780cd173a8050dc74
robertchou91 <robertchou91@gmail.com>
2020-01-04 12:54:10 -0600

    got rid of price filter

--- controllers/list/list.js
+++ controllers/list/list.js
@@ -1,12 +1,12 @@
 app.controller('listController', function ($scope, $http) {
 
-   $http.get("https://cargo-trailer-server.herokuapp.com/api/trailers")
+    $http.get("https://cargo-trailer-server.herokuapp.com/api/trailers")
         .then(function (response) {
             console.log("Successfully got Trailers");
             $scope.trailers = response.data;
         }, function (response) {
             console.log("Unable to get Trailers");
-        }); 
+        });
 
     $scope.delete = function (id) {
         $http({
@@ -20,65 +20,54 @@ app.controller('listController', function ($scope, $http) {
             console.log("trailer not deleted");
         });
     };
-  
-    $scope.refresh = function(){
+
+    $scope.refresh = function () {
         $http.get('https://cargo-trailer-server.herokuapp.com/api/trailers/')
-              .then(function(response){
-                   $scope.trailers = response.data;
-              });
+            .then(function (response) {
+                $scope.trailers = response.data;
+            });
     }
 
     $scope.Manufacturer = [
-        {"manufacturer" : "Arising"},
-        {"manufacturer" : "Big Tex Trailers"},
-        {"manufacturer" : "Little Tex Trailers"},
+        { "manufacturer": "Arising" },
+        { "manufacturer": "Big Tex Trailers" },
+        { "manufacturer": "Little Tex Trailers" },
     ];
 
     $scope.conditions = [
-        {"condition" : "New"},
-        {"condition" : "Used"},
-        {"condition" : "Cosmetic Damage"},
+        { "condition": "New" },
+        { "condition": "Used" },
+        { "condition": "Cosmetic Damage" },
     ];
 
     $scope.size = [
-        {"dimension" : "6X12"},
-        {"dimension" : "7X14"},
-        {"dimension" : "8.5X16"},
-        {"dimension" : "8.5X24"},
+        { "dimension": "6X12" },
+        { "dimension": "7X14" },
+        { "dimension": "8.5X16" },
+        { "dimension": "8.5X24" },
     ];
 
     $scope.years = [
-        {"year" : "2018"},
-        {"year" : "2019"},
-        {"year" : "2020"},
+        { "year": "2018" },
+        { "year": "2019" },
+        { "year": "2020" },
     ];
 
 
-//HTML slider
+    $scope.pagination = {
+        currentPage: 0,
+        pageSize: 5,
+        numberOfPages: function () {
+            return Math.ceil($scope.trailers.length / $scope.pagination.pageSize);
+        }
+    };
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+});
 
-//Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-output.innerHTML = this.value;
-}
-
-$scope.pagination = {
-    currentPage: 0,
-    pageSize: 5,
-    numberOfPages: function () {
-        return Math.ceil($scope.trailers.length / $scope.pagination.pageSize);
-    }
-};
-
-  });
-
-  app.filter('startFrom', function () {
-    if(typeof input === undefined){
+app.filter('startFrom', function () {
+    if (typeof input === undefined) {
         return null;
-    }else{
+    } else {
         return function (input, start) {
             start = +start; //parse to int
             return input.slice(start);
