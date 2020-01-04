adfc53e38424b16896a56acbb79181c308e5c1e5
robertchou91 <robertchou91@gmail.com>
2020-01-04 14:29:33 -0600

    edit page in progress

--- controllers/edit/edit.js
+++ controllers/edit/edit.js
@@ -1,3 +1,16 @@
-app.controller('editController', function($scope, $http) {
+app.controller('editController', function($scope, $http, $routeParams) {
+    // $scope.trailer = [];
+    var id = $routeParams.id;
+    console.log(id);
+    $scope.getById = function() {
+        $http.get("https://cargo-trailer-server.herokuapp.com/api/trailers/" + id)
+            .then(function(response) {
+                // console.log(response);
+                $scope.trailer = response.data;
+                console.log(response.data);
+            });
+    }
+    $scope.getById();
     
+        
 });
