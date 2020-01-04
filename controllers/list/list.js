efb7332468eb4fc1d8ac630886b8ee40bff69726
gbardales1 <gamalielbardales@live.com>
2020-01-04 02:28:36 -0600

    Create PAGE now sends post requests to node.js server

--- controllers/list/list.js
+++ controllers/list/list.js
@@ -1,13 +1,13 @@
 app.controller('listController', function ($scope, $http) {
 
-    $http.get("https://cargo-trailer-server.herokuapp.com/api/trailers")
+   $http.get("https://cargo-trailer-server.herokuapp.com/api/trailers")
         .then(function (response) {
             console.log(response.data);
             console.log("Successfully got Trailers");
             $scope.trailers = response.data;
         }, function (response) {
             console.log("Unable to get Trailers");
-        });
+        }); 
 
     $scope.delete = function (id) {
         $http({
