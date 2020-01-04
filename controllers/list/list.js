5aca387dee278b58d4e19302ef709f1476446b9a
Jonathan Zakian <jzakian12@gmail.com>
2020-01-04 12:37:58 -0600

    here are the filters I worked on

--- controllers/list/list.js
+++ controllers/list/list.js
@@ -23,10 +23,60 @@ app.controller('listController', function ($scope, $http) {
         });
     };
 
-    $scope.conditions = [
-        {"condition" : "new"},
-        {"condition" : "used"},
-        {"condition" : "damaged"},
+    $scope.Manufacturer = [
+        {"manufacturer" : "Arising"},
+        {"manufacturer" : "Big Tex Trailers"},
+        {"manufacturer" : "Little Tex Trailers"},
     ];
 
+    $scope.conditions = [
+        {"condition" : "New"},
+        {"condition" : "Used"},
+        {"condition" : "Cosmetic Damage"},
+    ];
+
+    $scope.size = [
+        {"dimension" : "6X12"},
+        {"dimension" : "7X14"},
+        {"dimension" : "8.5X16"},
+        {"dimension" : "8.5X24"},
+    ];
+
+    $scope.years = [
+        {"year" : "2018"},
+        {"year" : "2019"},
+        {"year" : "2020"},
+    ];
+
+
+//HTML slider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+//Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+output.innerHTML = this.value;
+}
+
+$scope.pagination = {
+    currentPage: 0,
+    pageSize: 5,
+    numberOfPages: function () {
+        return Math.ceil($scope.trailers.length / $scope.pagination.pageSize);
+    }
+};
+
+  });
+
+  app.filter('startFrom', function () {
+    if(typeof input === undefined){
+        return null;
+    }else{
+        return function (input, start) {
+            start = +start; //parse to int
+            return input.slice(start);
+        }
+    }
 });
