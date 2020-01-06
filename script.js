da5c923c5beca082a9deb9de3bc230380113499f
gbardales1 <gamalielbardales@live.com>
2020-01-06 16:14:50 -0600

    form validation on create page

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ $('.navbar-nav li a').on('click', function() {
     $(this).parent().parent().find('.active').removeClass('active');
     $(this).parent().addClass('active').css('font-weight', 'bold');
 });
+
+
