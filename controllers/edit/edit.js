09a2ac38f7ee1a3db242f3771946405ceebbe9a1
robertchou91 <robertchou91@gmail.com>
2020-01-06 20:42:33 -0600

    commit for now edit page

--- controllers/edit/edit.js
+++ controllers/edit/edit.js
@@ -1,49 +1,59 @@
-app.controller('editController', function($scope, $http, $routeParams,$window) {
-     $scope.trailer = [];
+app.controller('editController', function ($scope, $http, $routeParams, $window) {
+    $scope.trailer = [];
     var id = $routeParams.id;
     console.log(id);
-    $scope.getById = function() {
+
+    $scope.upload = function () {
+        var f = document.getElementById('file').files[0];
+        var r = new FileReader();
+        r.onloadend = function (e) {
+            $scope.image = r.result;
+        }
+        r.readAsDataURL(f);
+    }
+
+    $scope.getById = function () {
         $http.get("https://cargo-trailer-server.herokuapp.com/api/trailers/" + id)
-            .then(function(response) {
+            .then(function (response) {
                 // console.log(response);
                 $scope.trailer = response.data;
                 console.log(response.data);
             });
     }
     $scope.getById();
-    
-    $scope.submit= function(){
+
+    $scope.submit = function () {
         var data = {
-          
-              title:$scope.modelnum,
-              image: $scope.image,
-              manufacturer: $scope.manufacturer ,
-              price: $scope.amount,
-              model: $scope.modelnum ,
-              capacity: $scope.payload ,
-              dimension: $scope.dimension ,
-              condition: $scope.condition,
-              color: $scope.color ,
-              year: $scope.year ,
-              quantity: $scope.quantity
-  
-          }
-  
-  
+
+            title: $scope.modelnum,
+            image: $scope.image,
+            manufacturer: $scope.manufacturer,
+            price: $scope.amount,
+            model: $scope.modelnum,
+            capacity: $scope.payload,
+            dimension: $scope.dimension,
+            condition: $scope.condition,
+            color: $scope.color,
+            year: $scope.year,
+            quantity: $scope.quantity
+
+        }
+
+
         $http.put('https://cargo-trailer-server.herokuapp.com/api/trailers/' + id, (data)).then(function (response) {
-  
-          if (response.data)
-          
-          $scope.msg = "Post Data Submitted Successfully!";
-          console.log($scope.msg)
-          console.log(response.status)
-          console.log(response.data)
-          $window.location.href = '#!/list'
-  
-     })
-  }
-    
-        
+
+            if (response.data)
+
+                $scope.msg = "Post Data Submitted Successfully!";
+            console.log($scope.msg)
+            console.log(response.status)
+            console.log(response.data)
+            $window.location.href = '#!/list'
+
+        })
+    }
+
+
 });
 
 app.directive('myModelValue', function () {
